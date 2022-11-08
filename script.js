9a39b5451120228454a445bfe671937a498ca2f5
rportocarrero <portocarrero.ryan@gmail.com>
2022-11-08 09:42:18 -0500

    Added classifying model and bounding boxes
    Updated model classifying and bounding box code

--- script.js
+++ script.js
@@ -42,12 +42,52 @@ function enableCam(event){
     });
 }
 
+// Store model
+var model = undefined;
 
-//placehodler function
+cocoSsd.load().then(function(loadedModel){
+    model = loadedModel
+    // show model is ready to use
+    demosSection.classList.remove('invisible');
+})
+
+var children = []
+
 function predictWebcam(){
+    // classify frame in stream
+    model.detect(video).then(function(predictions){
+        // remove hilighting
+        for (let i=0; i< children.length; i++){
+            liveView.removeChild(children[1]);
+        }
+        children.splice(0);
 
+        //loop through predictions
+        for(let n = 0; n < predictions.length; n++){
+            if(predictions[n].score > 0.66){
+                const p = document.createElement('p');
+                p.innerText = predictions[n].class + ' - with '
+                    + Math.round(parseFloat(predictions[n].score) * 100)
+                    + '% confidence.';
+                p.style = 'margin-left' + predictions[n].bbox[0] + 'px; margin-top: '
+                    + (predictions[n].bbox[1] - 10) + 'px; width: '
+                    + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+                
+                    const highlighter = document.createElement('div');
+                    highlighter.setAttribute('class', 'hilighter');
+                    highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px: top: '
+                        + predictions[n].bbox[1] + 'px; width: '
+                        + preditcions[n].bbox[2] + 'px; height: '
+                        + predictions[n].bbox[3] + 'px;';
+
+                    liveView.appendChild(highlighter);
+                    liveView.appendChild(p);
+                    children.push(highlighter);
+                    children.push(p);
+            }
+    }
+    
+    //keep prediction
+    window.requestAnimationFrame(preductWebcam)
+});
 }
-
-//pretend model has loaded
-var model = true;
-demosSection.classList.remove('invisible');
